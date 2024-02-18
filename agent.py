41d534bfca5ac27d5915a92ae8be4d12547b3a4a
Jathin Narayan <jathin04official@gmail.com>
2024-02-18 16:40:14 +0530

    done plotting function

--- agent.py
+++ agent.py
@@ -5,6 +5,7 @@ from collections import deque
 import game
 from game import SnakeGameAI, Direction, Point
 from model import Linear_QNet, QTrainer
+from helper import plot
 
 MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
@@ -140,7 +141,11 @@ def train():
 
             print('Game: ', agent.n_games, ' Score: ', score, ' Record: ', record)
 
-            #todo: plots
+            plot_score.append(score)
+            total_score += score
+            mean_score = total_score / agent.n_games
+            plot_mean_score.append(mean_score)
+            plot(plot_score, plot_mean_score)
 
 
 if __name__ == '__main__':
